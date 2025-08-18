aa48f7166b03908072d5caef0561017aaf59389d
Loute <skaehdals822@gmail>
2025-08-18 11:36:24 +0900

    edit

--- sql/day250814_quiz_answer.sql
+++ sql/day250814_quiz_answer.sql
@@ -24,7 +24,7 @@ select  distinct car_id,
 from car_rental_company_rental_history a
 order by car_id desc;
 */
-
+/*
 with m as(
     select distinct car_id
     from car_rental_company_rental_history
@@ -39,3 +39,15 @@ select m.car_id,
     else '대여 가능' end as availability
 from m
 order by m.car_id desc;
+*/
+
+select car_id, 
+    case when
+        sum(
+            case
+                when '2022-10-16' between start_date and end_date then 1
+                else 0 end
+        )=1 then '대여중' else '대여 가능' end as availability
+from car_rental_company_rental_history
+group by car_id
+order by car_id desc;
